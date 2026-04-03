e4b24eabc1a58aeaa1763cd80634d7eb5c46034b
TonyWu20 <tony.w21@gmail.com>
2026-04-03 10:41:10 +0800

    Update env

--- nushell/default.nix
+++ nushell/default.nix
@@ -75,11 +75,13 @@
 
           $env.YUNWU_TOKEN = (open ${config.sops.secrets.yunwu_claude_api.path})
           $env.POE_TOKEN = (open ${config.sops.secrets.poe_chatbot_api.path})
+          $env.FOXCODE_TOKEN = (open ${config.sops.secrets.foxcode_claude_token})
           $env.POE_BASE_URL = "https://api.poe.com"
           $env.YUNWU_BASE_URL = "https://yunwu.ai"
+          $env.FOXCODE_BASE_URL = "https://code.newcli.com/claude/droid"
           $env.ANTHROPIC_API_KEY = ""
-          $env.ANTHROPIC_AUTH_TOKEN = $env.YUNWU_TOKEN
-          $env.ANTHROPIC_BASE_URL = $env.YUNWU_BASE_URL
+          $env.ANTHROPIC_AUTH_TOKEN = $env.FOXCODE_TOKEN
+          $env.ANTHROPIC_BASE_URL = $env.FOXCODE_BASE_URL
 
           $env.config = $current
           source ~/.zoxide.nu
