2ae222763323dc3874efaf7fa93e1e8d1b9a09ec
TonyWu20 <tony.w21@gmail.com>
2026-04-04 11:16:55 +0800

    remove apis

--- nushell/default.nix
+++ nushell/default.nix
@@ -77,15 +77,6 @@
           $current.completions.external = ($current.completions.external
           | default true enable
           | default { $carapace_completer } completer)
-          $env.YUNWU_TOKEN = (open ${config.sops.secrets.yunwu_claude_api.path})
-          $env.POE_TOKEN = (open ${config.sops.secrets.poe_chatbot_api.path})
-          $env.FOXCODE_TOKEN = (open ${config.sops.secrets.foxcode_claude_token.path})
-          $env.POE_BASE_URL = "https://api.poe.com"
-          $env.YUNWU_BASE_URL = "https://yunwu.ai"
-          $env.FOXCODE_BASE_URL = "https://code.newcli.com/claude/droid"
-          $env.ANTHROPIC_API_KEY = ""
-          $env.ANTHROPIC_AUTH_TOKEN = $env.FOXCODE_TOKEN
-          $env.ANTHROPIC_BASE_URL = $env.FOXCODE_BASE_URL
 
           $env.config = $current
           source ~/.zoxide.nu
